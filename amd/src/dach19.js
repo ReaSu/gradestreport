8515434d1bfba222b0d3fd68b46c04dd568bd21e
Rea Sutter <sutter@technikum-wien.at>
2019-09-11 15:40:11 +0200

    class userfield is excluded from css changes

--- amd/src/dach19.js
+++ amd/src/dach19.js
@@ -3,21 +3,23 @@ define(['jquery'], function($) {
     return {
         init: function() {
 
-            $(".cell.topleft.cell.c0").html('<select id="widthSelector"></select>');
 
-            $(document).ready(function(){
-                $("<option/>").val("wide").text("Wide").appendTo("#widthSelector");
+             $(".cell.topleft.cell.c0").html('<select id="widthSelector"></select>');
+
+            $(document).ready(function() {
+                $("<option/>").val("wide").text("Standard").appendTo("#widthSelector");
             });
-            $(document).ready(function(){
-                $("<option/>").val("narrow").text("Narrow").appendTo("#widthSelector");
+            $(document).ready(function() {
+                $("<option/>").val("slim").text("Slim").appendTo("#widthSelector");
             });
 
-            $('#widthSelector').change(function(){
-                if ($('#widthSelector').val() === 'narrow') {
+
+            $('#widthSelector').change(function() {
+                if ($('#widthSelector').val() === 'slim') {
                     $('.item.catlevel2.highlightable.cell').css({
                         "writing-mode": "vertical-lr"
                     });
-                    $('.cell').not('.user, .userreport').css({
+                    $('.cell').not('.user, .userreport, .userfield').css({
                         "max-width": "40px",
                         "min-width": "40px"
                     });
@@ -25,7 +27,7 @@ define(['jquery'], function($) {
                     $('.item.catlevel2.highlightable.cell').css({
                         "writing-mode": "horizontal-tb",
                     });
-                    $('.cell').not('.user, .userreport').css({
+                    $('.cell').not('.user, .userreport, .userfield').css({
                         "max-width": "400px",
                         "min-width": "100px"
                     });
